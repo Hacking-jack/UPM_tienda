5b873f5467883b384441e93bc306816446f43786
Daniel Rodriguez <daniel.rodriguezti1@gmail.com>
2025-09-29 19:16:11 +0200

    clases iniciales de producto y ticket generadas

--- src/main/java/es/upm/etsisi/poo/models/Product.java
+++ src/main/java/es/upm/etsisi/poo/models/Product.java
@@ -5,4 +5,42 @@ public class Product {
     private String description;
     private int price;
 
+    public Product(String name, int price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
--- src/main/java/es/upm/etsisi/poo/models/Ticket.java
+++ src/main/java/es/upm/etsisi/poo/models/Ticket.java
@@ -8,4 +8,27 @@ public class Ticket {
     private final LocalDateTime creationDateTime = LocalDateTime.now();
     private String cashier;
 
+    public Ticket(String cashier) {
+        this.cashier = cashier;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+
+    public LocalDateTime getCreationDateTime() {
+        return creationDateTime;
+    }
+
+    public String getCashier() {
+        return cashier;
+    }
+
+    public void setCashier(String cashier) {
+        this.cashier = cashier;
+    }
 }
