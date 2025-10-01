c7be9c64cfa84e65cfb6d83ef88fbc404dbea361
Usuario <juliofernandezdeluz@gmail.com>
2025-10-01 18:08:45 +0200

    Añadido echo a App

--- src/main/java/es/upm/etsisi/poo/view/App.java
+++ src/main/java/es/upm/etsisi/poo/view/App.java
@@ -78,6 +78,9 @@ public class App
                     break;
                 case "print":
                     break;
+                case "echo":
+                    System.out.println(line);
+                    break;
                 default:
                     break;
             }
