4fd9fb6d560147f62114fa609e3f1b62b03f734a
Usuario <juliofernandezdeluz@gmail.com>
2025-10-01 17:01:26 +0200

    Arreglos menores

--- src/main/java/es/upm/etsisi/poo/models/Ticket.java
+++ src/main/java/es/upm/etsisi/poo/models/Ticket.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 
 public class Ticket {
-    private static ArrayList<Product> products;
+    private ArrayList<Product> products;
 
 
 
@@ -26,14 +26,14 @@ public class Ticket {
 
 
     //Despues decidimos donde va
-    public static void add(Product product, int cantidad){
+    public void add(Product product, int cantidad){
         for(int i = 0; i<cantidad;i++)
             products.add(product);
     }
-    public static void remove(Product product){
+    public void remove(Product product){
         products.remove(product);
     }
-    public static void print(){ // completar
+    public void print(){ // completar
         for (int i = 0; i < products.size(); i++) {
             System.out.println(products.get(i).toString());
         }
