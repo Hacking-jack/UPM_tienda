b6e7d86e9ecd07a3153a63683848bba63d1aa028
laxanti <sanduman2049@gmail.com>
2025-10-01 17:52:46 +0200

    He creado el metodo existeId, y el metodo remove, y he arreglado los problemas que habia con los metodos de la clase ProductController

--- src/main/java/es/upm/etsisi/poo/controler/ProductController.java
+++ src/main/java/es/upm/etsisi/poo/controler/ProductController.java
@@ -1,4 +1,5 @@
 package es.upm.etsisi.poo.controler;
+
 import java.util.ArrayList;
 
 import es.upm.etsisi.poo.models.Product;
@@ -13,47 +14,72 @@ public class ProductController {
 
     private ArrayList<Product> products;
 
-    public ProductController(){
-        products= new ArrayList<>();
+    public ProductController() {
+        products = new ArrayList<>();
     }
 
 
-    public void add(int id, String name, String categories, double price){
-        if (products.contains(id)){
+    public void add(int id, String name, String categories, double price) {
+        if (existeId(id) != -1) {
             System.out.println("Error. Ya existe el id.");
-        }else{
+        } else {
             Product product = new Product(name, price, categories, id);
             products.add(product);
             System.out.println(product.toString());
         }
         //Falta app.echo
     }
-    public void list(){
-        for (int i=0;i<products.size();i++){
-            Product p = products.get(i);
-            System.out.println(p.toString());
+
+    public void list() {
+        System.out.println("Catalog:");
+        for (int i = 0; i < products.size(); i++) {
+            System.out.println(products.get(i).toString());
         }
     }
-    public void update(int id, String campo, String valor){
-        Product product = products.get(id);
-        if (!(product==null)){
+
+    public void update(int id, String campo, String valor) {
+        int position = existeId(id);
+        if (existeId(id) == -1) {
             System.out.println("Error. No existe el id para actualizar.");
-        }else{
-            switch (campo){
+        } else {
+            switch (campo) {
                 case "NAME":
-                    product.setName(valor);
+                    products.get(position).setName(valor);
                     break;
                 case "CATEGORY":
-                    product.setCategories(valor);
+                    products.get(position).setCategories(valor);
                     break;
                 case "PRICE":
-                    product.setPrice(Double.parseDouble(valor));
+                    products.get(position).setPrice(Double.parseDouble(valor));
                     break;
                 default:
                     System.out.println("Campo no valido");
                     break;
             }
         }
-    }//Aqui falta un trycatch
+    }
+
+    public void remove(int id) {
+        int position = existeId(id);
+        if (position == -1) {
+            System.out.println("Error. No existe el id para borrar.");
+        } else {
+            products.remove(products.get(position));
+        }
+    }
+
+    public int existeId(int id) {
+        boolean encontrado = false;
+        int i = 0;
+        while (!encontrado && i < products.size()) {
+            if (products.get(i).getId() == id) {
+                encontrado = true;
+            }
+        }
+        if (!encontrado) {
+            i = -1;
+        }
+        return i;
+    }
 
 }
