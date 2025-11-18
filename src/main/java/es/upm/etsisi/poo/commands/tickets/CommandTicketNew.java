067933e1990b88d4375077755b2e2cde712abf61
Usuario <juliofernandezdeluz@gmail.com>
2025-11-18 14:21:01 +0100

    añadido esqueleto estructura Command

--- src/main/java/es/upm/etsisi/poo/commands/tickets/CommandTicketNew.java
+++ src/main/java/es/upm/etsisi/poo/commands/tickets/CommandTicketNew.java
@@ -3,12 +3,12 @@ package es.upm.etsisi.poo.commands.tickets;
 import es.upm.etsisi.poo.commands.Command;
 
 public class CommandTicketNew implements Command {
-    private final String providedId; // null si no se da
+    private final String id; // null si no se da
     private final String cashId;
     private final String userId;
 
-    public CommandTicketNew(String providedId, String cashId, String userId) {
-        this.providedId = providedId;
+    public CommandTicketNew(String id, String cashId, String userId) {
+        this.id = id;
         this.cashId = cashId;
         this.userId = userId;
     }
