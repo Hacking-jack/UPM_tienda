85b293c92aa0e69d285557225fdd49f1ddff95dc
Rafael Caro <rafael.caroancochea@alumnos.upm.es>
2025-11-24 18:37:24 +0100

    execute de CommandTicketAddProduct

--- src/main/java/es/upm/etsisi/poo/commands/tickets/CommandTicketAddProduct.java
+++ src/main/java/es/upm/etsisi/poo/commands/tickets/CommandTicketAddProduct.java
@@ -1,5 +1,6 @@
 package es.upm.etsisi.poo.commands.tickets;
 
+import es.upm.etsisi.poo.BASES_DE_DATOS.ProductDB;
 import es.upm.etsisi.poo.commands.Command;
 import es.upm.etsisi.poo.controler.ProductController;
 import es.upm.etsisi.poo.controler.TicketController;
@@ -7,14 +8,14 @@ import es.upm.etsisi.poo.controler.TicketController;
 public class CommandTicketAddProduct implements Command {
     private final String ticketId;
     private final String cashId;
-    private final String productId;
+    private final int productId;
     private final int amount;
     private final String[] pers; // --p<txt>
     private final TicketController ticketController;
     private final ProductController productController;
 
 
-    public CommandTicketAddProduct(String ticketId, String cashId, String productId, int amount, String[] pers,
+    public CommandTicketAddProduct(String ticketId, String cashId, int productId, int amount, String[] pers,
                                    TicketController ticketController, ProductController productController) {
         this.ticketId = ticketId;
         this.cashId = cashId;
@@ -26,11 +27,11 @@ public class CommandTicketAddProduct implements Command {
     }
     //TODO existe esta otacion que te sale en intellij como una lista de tareas
     @Override
-    public boolean execute() {
+    public boolean execute() {//hacer dos add, uno con personalizacion y otro sin
         if(pers[0]==null) {// sin pers
-            ticketController.add()//Lo dejo asi para que no se nos olvide hacerlo
+            ticketController.addProductPers(ProductDB.findId(productId), amount, pers);//Lo dejo asi para que no se nos olvide hacerlo
         }else{
-            ticketController. //con pers
+            ticketController.addProduct(ProductDB.findId(productId), amount);//con pers
         }
         return true;
     }
